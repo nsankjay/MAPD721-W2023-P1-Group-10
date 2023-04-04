572a6c8f5620274e1ed6b45de333f131448f73c0
nsankjay <113073827+nsankjay@users.noreply.github.com>
2023-04-04 10:05:40 -0400

    Created Project file

---
+++ app/src/test/java/com/sn/mapd721_w2023_p1_group_10/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.sn.mapd721_w2023_p1_group_10
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
