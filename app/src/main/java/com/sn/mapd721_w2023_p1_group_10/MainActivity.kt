a7cfde82fcdc3cc880d38c3a5f26768c31ca487d
nsankjay <113073827+nsankjay@users.noreply.github.com>
2023-04-06 11:35:38 -0400

    Text design completed in Main view

--- app/src/main/java/com/sn/mapd721_w2023_p1_group_10/MainActivity.kt
+++ app/src/main/java/com/sn/mapd721_w2023_p1_group_10/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.sn.mapd721_w2023_p1_group_10
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val editBtn=findViewById<Button>(R.id.goToWeatherViewBtn)
+
+        //Click Edit Button action - go to edit user account details page
+        editBtn.setOnClickListener {
+            Intent(this, MainWeatherViewActivity::class.java).also {
+
+                startActivity(it)
+            }
+
+        }
+
     }
+
 }
---
+++ app/src/main/java/com/sn/mapd721_w2023_p1_group_10/MainWeatherViewActivity.kt
@@ -0,0 +1,11 @@
+package com.sn.mapd721_w2023_p1_group_10
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainWeatherViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_weather_view)
+    }
+}
